523c88508712a28014b4ebb1a9d2972fcbb674b8
Fredpwol <fredpwol@gmail.com>
2022-01-09 00:48:24 +0100

    finished problem

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -2,19 +2,23 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
     let mut magazine_word_usage: HashMap<&str, u32> = HashMap::new();
-    for word in magazine{
-        magazine_word_usage.insert(word, 0);
+    for word in magazine {
+        magazine_word_usage
+            .entry(word)
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
     }
-    for word in note{
-        if !magazine_word_usage.contains_key(word) || *magazine_word_usage.get(word).unwrap()!= 0 as u32 {
-            return false
+    for word in note {
+        if !magazine_word_usage.contains_key(word)
+            || *magazine_word_usage.get(word).unwrap() == 0 as u32
+        {
+            return false;
         }
-        magazine_word_usage.entry(word).insert(1);
+        magazine_word_usage.entry(word).and_modify(|e| *e -= 1);
     }
     return true;
-
 }
