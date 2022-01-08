00a9a1f5128053fadebd2d6e0611d800feb297f7
Fredpwol <fredpwol@gmail.com>
2022-01-08 20:48:06 +0100

    solved entry api problem

---
+++ magazine-cutout/src/lib.rs
@@ -0,0 +1,20 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::{collections::HashMap};
+
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
+    let mut magazine_word_usage: HashMap<&str, u32> = HashMap::new();
+    for word in magazine{
+        magazine_word_usage.insert(word, 0);
+    }
+    for word in note{
+        if !magazine_word_usage.contains_key(word) || *magazine_word_usage.get(word).unwrap()!= 0 as u32 {
+            return false
+        }
+        magazine_word_usage.entry(word).insert(1);
+    }
+    return true;
+
+}
