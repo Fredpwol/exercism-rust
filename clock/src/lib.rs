e31faece26908efde8f047b2954f8c30d2037259
Fredpwol <fredpwol@gmail.com>
2022-01-15 21:58:57 +0100

    solved clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,4 +1,7 @@
-use std::fmt;
+
+
+use std::fmt; 
+
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
@@ -9,22 +12,20 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Clock{
-            hours,
-            minutes
+            hours: (hours + (minutes as f32/60 as f32).floor() as i32).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock { minutes: (self.minutes + minutes) % 60, hours: (self.hours + minutes / 60) as i32}
-    }
-
-    pub fn to_string(&self) -> String{
-        format!("{:00$}{:01$}", self.hours as usize, self.minutes as usize)
+        Clock { 
+            minutes: (self.minutes + minutes).rem_euclid(60) , 
+            hours: (self.hours  + ((self.minutes + minutes) as f32 / 60 as f32).floor() as i32).rem_euclid(24) }
     }
 }
 
 impl fmt::Display for Clock{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}:{}", self.hours, self.minutes)
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
